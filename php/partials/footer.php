7845366644302d45239efedb51e2214faf7ca36c
mizukikaihatsu <dudet7e@gmail.com>
2021-09-28 16:19:11 +0900

    first commit

---
+++ php/partials/footer.php
@@ -0,0 +1,19 @@
+<?php
+namespace partials;
+
+function footer() {
+?>
+            </main>
+            <footer class="text-center pt-5">
+            <img width="30" src="images/nezumizuki-logo.png" alt="" class="mr-2" />
+                <span class="text-muted font-weight-bold align-bottom"> &copy; NEZUMIZUKI</span>
+            </footer>
+        </div>
+        <script src="<?php echo BASE_JS_PATH ?>vendor/chart.js"></script>
+        <script src="<?php echo BASE_JS_PATH ?>pie-chart.js"></script>
+        <script src="<?php echo BASE_JS_PATH ?>form-validate.js"></script>
+    </body>
+    </html>
+<?php 
+} 
+?>
